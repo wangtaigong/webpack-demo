8f96e070b31c38d8a4222e24a354207b023fff90
张荣彪 <zhangrongbiao@tongzhi.com.cn>
2020-05-13 11:26:41 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+import _ from 'lodash'
 function component() {
   let element = document.createElement('div')
 
-  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
 
   return element
