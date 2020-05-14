1b6201a9d01e1f8d9c1cc8d1b6a75781058e7e03
张荣彪 <zhangrongbiao@tongzhi.com.cn>
2020-05-14 09:36:05 +0800

    update asset

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,36 @@ const path = require('path')
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: [
+          'csv-loader'
+        ]
+      },
+      {
+        test: /\.(xml)$/,
+        use: [
+          'xml-loader'
+        ]
+      }
+    ]
   }
 }
