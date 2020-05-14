f3753d69fcb5314cc92ae92e3fce234e2f38fc57
张荣彪 <zhangrongbiao@tongzhi.com.cn>
2020-05-14 10:46:43 +0800

    development done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import _ from 'lodash'
+import printMe from './print.js'
 
 function component() {
   let element = document.createElement('div')
+  var btn = document.createElement('button')
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+
+  btn.innerHTML = 'click here, then console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element
 }
