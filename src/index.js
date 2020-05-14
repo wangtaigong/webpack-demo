d14610cd1f38ef18a5781bb980698813ea91ffd6
张荣彪 <zhangrongbiao@tongzhi.com.cn>
2020-05-14 11:33:24 +0800

    start up

--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
 import _ from 'lodash'
-import printMe from './print.js'
-import './styles.css'
 
 function component() {
-  let element = document.createElement('div')
-  var btn = document.createElement('button')
+  let element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  btn.innerHTML = 'click here, then console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-  return element
+  return element;
 }
 
-document.body.appendChild(component())
-
-if (module.hot) {
-  module.hot.accept('./print.js', function() {
-    console.log('Accepting the updated printMe module!')
-    printMe()
-  })
-}
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,9 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const  { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const webpack = require('webpack')
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    app: './src/index.js'
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'output management'
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ],
+  entry: './src/index.js',
   output: {
-    filename: '[name].bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 }
